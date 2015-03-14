87f779f7b0a36b1d6dba9cb7144b1f3617d451c6
Vegard Langas <sjefen6@gmail.com>
2015-03-14 23:51:29 +0100

    Fixes linebreaks in video description

--- ytms.php
+++ ytms.php
@@ -30,6 +30,8 @@ if (empty($_GET['username'])){
 		parse_str($linkQuery, $linkQueryParams);
 		$videoId = $linkQueryParams["v"];
 		
+		$feed->entry[$i]->content = str_replace('font-size: 12px; margin: 3px 0px;','font-size: 12px; margin: 3px 0px; white-space: pre-wrap;',$feed->entry[$i]->content);
+		
 		$feed->entry[$i]->content = "<iframe id=\"ytplayer\" type=\"text/html\" width=\"640\" height=\"390\" " .
 			"src=\"http://www.youtube.com/embed/" . $videoId . "?autoplay=0&amp;origin=http://feedfix.gbt.cc\" " .
 			"frameborder=\"0\" allowfullscreen></iframe>\n<br>" . $feed->entry[$i]->content;
